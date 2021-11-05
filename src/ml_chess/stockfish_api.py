1634b1c613aee1627c0b9734bf0a9e96c9e76144
Ian Cowan <ian@cowanemail.com>
2021-11-05 13:02:34 -0400

    Begin writing stockfish api for our use

--- src/ml_chess/stockfish_api.py
+++ src/ml_chess/stockfish_api.py
@@ -5,5 +5,11 @@ fish = Stockfish()
 def get_best_move():
     return fish.get_best_move()
 
+def make_move(move):
+    fish.make_moves_from_current_position([move])
+
 def get_fish():
     return fish
+
+def get_board():
+    return fish.get_fen_position()
