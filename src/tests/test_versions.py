e1e5fa9f41c7d388486ff6fde32e7e411903145b
Ian Cowan <ian@cowanemail.com>
2021-11-05 12:47:24 -0400

    Fix versions, add .tool-versions

--- src/tests/test_versions.py
+++ src/tests/test_versions.py
@@ -3,22 +3,12 @@ import os
 def test_python3_version():
     stream = os.popen('python3 --version')
     version = stream.read().split(' ')[1].strip()
-    assert version == '3.8.10'
+    assert version == tool_version('python')
 
 def test_pip_version():
     stream = os.popen('pip --version')
     version = stream.read().split(' ')[1].strip()
-    assert version == '20.0.2'
-
-def test_pip_stockfish_version():
-    stream = os.popen('pip show stockfish')
-    version = stream.read().split('\n')[1].split(' ')[1].strip()
-    assert version == '3.17.0'
-
-def test_pip_pytest_version():
-    stream = os.popen('pip show pytest')
-    version = stream.read().split('\n')[1].split(' ')[1].strip()
-    assert version == '6.2.5'
+    assert version == tool_version('pip')
 
 def test_stockfish_version():
     stream = os.popen('stockfish compiler')
@@ -28,3 +18,14 @@ def test_stockfish_version():
     version_p2 = stream_split_0_split[2].strip()
     assert version_p1 == '11'
     assert version_p2 == '64'
+
+def tool_version(app):
+    cwd = os.getcwd()
+    tool_versions = open(cwd + '/../../.tool-versions', 'r')
+
+    for line in tool_versions:
+        line_split = line.split(' ')
+        if line_split[0] == app:
+            return line_split[1].strip()
+
+    return ''
